8ce6541dcff7dc684fdc4cc12610c60c9715b5dd
Euan Mendoza <bebopbamf@effectfree.dev>
2025-03-18 12:02:26 +1100

    solutions to challenge 1

--- week_05/01_two_sum/two_sum.cpp
+++ week_05/01_two_sum/two_sum.cpp
@@ -1,16 +1,13 @@
 #include "two_sum.hpp"
 
 std::pair<int, int> twoSum(const std::vector<int>& vec, int target) {
-	std::ignore = vec;
-	std::ignore = target;
+	std::unordered_map<int, std::size_t> vals{};
 
-	DEBUG_LOG("Test Logging!");
-	DEBUG_LOG_LIST(vec);
-
-	std::unordered_set vals{};
-
-	for (const int& n : vec) {
-		if (vals.contains)
+	for (std::size_t i = 0; i < vec.size(); i++) {
+		if (vals.contains(target - vec.at(i))) {
+			return {vals.at(target - vec.at(i)), i};
+		}
+		vals.insert({vec.at(i), i});
 	}
 
 	return {0, 0};
