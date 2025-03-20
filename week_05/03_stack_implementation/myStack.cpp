e51b40a68afadcde91a3b116f8b01e322f382a29
Euan Mendoza <bebopbamf@effectfree.dev>
2025-03-21 00:21:59 +1100

    updated stack implementation

---
+++ week_05/03_stack_implementation/myStack.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <string>
+#include "myStack.hpp"
+
+// default constructor (nothing to do here)
+template <typename T>
+MyStack<T>::MyStack() {}
+
+// add an element to the top of stack
+template <typename T>
+void MyStack<T>::push(T val) {
+  data.push_back(val);
+}
+
+// remove an element from the top of stack
+template <typename T>
+void MyStack<T>::pop() {
+  data.pop_back();
+}
+
+// get the element at the top of stack
+template <typename T>
+T& MyStack<T>::top() {
+  return data.at(size() - 1);
+}
+
+// return number of elements in the stack
+template <typename T>
+int MyStack<T>::size() {
+  return data.size();
+}
+
+// check if stack is empty
+template <typename T>
+bool MyStack<T>::empty() {
+  return data.empty();
+}
+
+template class MyStack<int>;
+template class MyStack<char>;
+template class MyStack<std::string>;
